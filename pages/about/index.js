04427da307808898bb50de24ec41eca158ada258
bwahlberg <bwahlberg@care.com>
2021-02-09 15:01:43 -0500

    feat(1): test about

---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+        <h2 className={styles.title}>
+          About
+        </h2>
+    </main>
+  );
+}
